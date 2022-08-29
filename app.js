26fb31859f2d8090f3908c7b21a5f9691457b964
Israel-Felipe <israelfelipet@gmail.com>
2022-08-28 22:43:15 -0300

    post tweet

--- app.js
+++ app.js
@@ -27,9 +27,21 @@ app.post('/sign-up', (req, res) => {
     res.send("OK");
 })
 
+app.post('/tweets', (req, res) => {
+    const tweet = req.body
+
+    tweets.push(tweet);
+    res.send("OK");
+})
+
 app.get('/sign-up', (req, res) => {
 
     res.send(usuarios)
+})
+
+app.get('/tweets', (req, res) => {
+
+    res.send(tweets)
 })
 
 app.listen(5000, () => console.log('Server listening on port 5000'));
