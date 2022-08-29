6c87147ee15a9064bd3c0d63135c069bfda626f5
Israel-Felipe <israelfelipet@gmail.com>
2022-08-28 22:37:41 -0300

    post sign-up

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const usuarios = [
+    {
+        username: 'bobesponja', 
+        avatar: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info'
+    }
+];
+
+const tweets = [
+    {
+        username: 'bobesponja',
+        tweet: 'eu amo o hub'
+    }
+];
+
+app.post('/sign-up', (req, res) => {
+    const usuario = req.body
+
+    usuarios.push(usuario);
+    res.send("OK");
+})
+
+app.get('/sign-up', (req, res) => {
+
+    res.send(usuarios)
+})
+
+app.listen(5000, () => console.log('Server listening on port 5000'));
